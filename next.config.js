1eef26301b5045bec995fcccc6321d1b24fecd83
MarcioParanhos <marcio.naga@gmail.com>
2026-08-26 14:54:59 -0300

    :recycle: refactor: streamline environment variable configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://alexia-camara-app.vercel.app",
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || "https://alexia-camara-app.vercel.app",
+  },
 };
-
-env: {
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://alexia-camara-app.vercel.app',
-  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://alexia-camara-app.vercel.app',
-}
 
 module.exports = nextConfig;
